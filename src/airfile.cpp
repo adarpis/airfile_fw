64dc1c18c4d73134b64dc1b63123e445ef200d64
Adrian Saldana <adrian.d.arpi.s1@gmail.com>
2022-09-18 03:16:00 -0300

    Deep sleep mode
    feat:
    - Add deep sleep mode configuration
    issue:
    - Unable to test because USB CDC built-in reset

--- src/airfile.cpp
+++ src/airfile.cpp
@@ -1,12 +1,12 @@
 /**
  * @file airfile.cpp
  * @author your name (you@domain.com)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2022-09-17
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 
 #include <stdio.h>
@@ -19,7 +19,7 @@
 /* Can run 'make menuconfig' to choose the GPIO to blink,
    or you can edit the following line and set a number here.
 */
-#define BLINK_GPIO (gpio_num_t)CONFIG_BLINK_GPIO
+#define BLINK_GPIO (gpio_num_t) CONFIG_BLINK_GPIO
 
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 8
@@ -36,7 +36,8 @@ void blink_task(void *pvParameter)
     gpio_pad_select_gpio(BLINK_GPIO);
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-    while(1) {
+    while (1)
+    {
         /* Blink off (output low) */
         gpio_set_level(BLINK_GPIO, 0);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
@@ -46,10 +47,43 @@ void blink_task(void *pvParameter)
     }
 }
 
+#define uS_TO_S_FACTOR 1000000 /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP 5        /* Time ESP32 will go to sleep (in seconds) */
+
+void print_wakeup_reason()
+{
+    esp_sleep_wakeup_cause_t wakeup_reason;
+    wakeup_reason = esp_sleep_get_wakeup_cause();
+
+    switch (wakeup_reason)
+    {
+    case ESP_SLEEP_WAKEUP_EXT0:
+        Serial.println("Wakeup caused by external signal using RTC_IO");
+        break;
+    case ESP_SLEEP_WAKEUP_EXT1:
+        Serial.println("Wakeup caused by external signal using RTC_CNTL");
+        break;
+    case ESP_SLEEP_WAKEUP_TIMER:
+        Serial.println("Wakeup caused by timer");
+        break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD:
+        Serial.println("Wakeup caused by touchpad");
+        break;
+    case ESP_SLEEP_WAKEUP_ULP:
+        Serial.println("Wakeup caused by ULP program");
+        break;
+    default:
+        Serial.printf("Wakeup was not caused by deep sleep: %d\n", wakeup_reason);
+        break;
+    }
+}
+
 #if !CONFIG_AUTOSTART_ARDUINO
-void arduinoTask(void *pvParameter) {
+void arduinoTask(void *pvParameter)
+{
     pinMode(LED_BUILTIN, OUTPUT);
-    while(1) {
+    while (1)
+    {
         digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
         delay(1000);
     }
@@ -64,14 +98,38 @@ extern "C" void app_main()
     xTaskCreate(&arduinoTask, "arduino_task", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
 }
 #else
-void setup() {
+void setup()
+{
     Serial.begin(115200);
+    while (!Serial)
+    {
+        ; // wait for serial port to connect. Needed for native USB
+    }
     xTaskCreate(&blink_task, "blink_task", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
     pinMode(LED_BUILTIN, OUTPUT);
+
+    print_wakeup_reason(); // Print the wakeup reason for ESP32
+
+    delay(5000);
+
+    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR); // ESP32 wakes up every 5 seconds
+
+    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_ON); // all RTC Peripherals are powered
 }
-void loop() {
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-    Serial.println("Hello!");
-    delay(250);
+
+void loop()
+{
+    static uint8_t i = 0;
+    while (i < 24)
+    {
+        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+        Serial.println("Hello!");
+        delay(250);
+        i++;
+    }
+
+    Serial.println("Going to deep-sleep now");
+    Serial.flush();
+    esp_deep_sleep_start();
 }
 #endif
