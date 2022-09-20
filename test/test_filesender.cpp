625b780a692dcfaf25e3b87f925b510eeb6a3af3
Adrian Saldana <adrian.d.arpi.s1@gmail.com>
2022-09-20 00:41:22 -0300

    Test filesender lib
    debug:
    - Add / to test paths
    feat:
    - Add LED testing, turn on while testing
    test:
    - Add sub tests to initFileSender test
    - Add lib_deps to test environment

--- test/test_filesender.cpp
+++ test/test_filesender.cpp
@@ -17,24 +17,29 @@
 #define LED_BUILTIN 8
 #endif
 
-const char *FILE_PATH_TO_TEST_AVAILABLE = "500KB.out";
-const char *FILE_PATH_TO_TEST_UNAVAILABLE = "unavailable.txt";
+const char *FILE_PATH_TO_TEST_AVAILABLE = "/500KB.out";
+const char *FILE_PATH_TO_TEST_UNAVAILABLE = "/spiffs/unavailable.txt";
 char path_variable[10];
 
 void setUp(void)
 {
     // set stuff up here
     strcpy(path_variable, FILE_PATH_TO_TEST_AVAILABLE);
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void tearDown(void)
 {
     // clean stuff up here
+    digitalWrite(LED_BUILTIN, LOW);
 }
 
 void test_initFileSender(void)
 {
-    TEST_ASSERT_MESSAGE(initFileSender(FILE_PATH_TO_TEST_AVAILABLE), "File available in SPDIFF partition");
+    TEST_ASSERT_FALSE_MESSAGE(initFileSender(FILE_PATH_TO_TEST_AVAILABLE), "Unexpected file unavailable in SPDIFF partition");
+    TEST_ASSERT_TRUE_MESSAGE(initFileSender(FILE_PATH_TO_TEST_UNAVAILABLE), "Unexpended file available in SPDIFF partition");
+    TEST_ASSERT_FALSE_MESSAGE(initFileSender(path_variable), "Unexpected file unavailable in SPDIFF partition, path from RAM");
 }
 
 void setup()
